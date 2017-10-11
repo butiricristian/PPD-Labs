f4c9bc7471eea2e851d8a22c906007fb4237b503
Butiri Cristian <butiri.cristian@gmail.com>
2017-10-11 23:59:27 +0300

    small changes

--- Lab1/Main.py
+++ Lab1/Main.py
@@ -1,8 +1,11 @@
+import threading
+
 import numpy as np
 
+res = []
 
 def work_split(nrTasks, nrWorkers):
-    return (nrTasks/nrWorkers, nrTasks%nrWorkers)
+    return (nrTasks / nrWorkers, nrTasks % nrWorkers)
 
 
 def user_input():
@@ -10,6 +13,12 @@ def user_input():
     N = int(input("N= "))
     K = int(input("K= "))
     M = int(input("M= "))
+    global res
+    res = [[0 for _ in range(N)] for _ in range(M)]
+
+
+def worker(matrix1, matrix2, a, b):
+    res[a:b] = np.dot(matrix1[a:b], matrix2)
 
 
 def solve(nrWorkers, N, K, M):
@@ -17,9 +26,12 @@ def solve(nrWorkers, N, K, M):
     matrix2 = np.random.random_integers(100, size=(K, M))
     print(matrix1, "\n", matrix2)
     interval, lastInterval = work_split(N, nrWorkers)
-
-    for i in range(nrWorkers):
-        res = np.dot(matrix1[i:(i+interval)], matrix2)
-
-
-
+    threads = []
+    for i in range(nrWorkers - 1):
+        t = threading.Thread(target=worker, args=(matrix1, matrix2, i * interval, (i + 1) * interval))
+        threads.append(t)
+        t.start()
+    t = threading.Thread(target=np.dot,
+                         args=(matrix1[(nrWorkers - 1) * interval:(nrWorkers - 1) * interval + lastInterval], matrix2))
+    threads.append(t)
+    t.start()
