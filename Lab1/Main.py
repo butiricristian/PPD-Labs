9d39b55d81d74baa06e56e920336296322465f1f
Butiri Cristian <butiri.cristian@gmail.com>
2017-10-12 17:21:15 +0300

    finalizing lab

--- Lab1/Main.py
+++ Lab1/Main.py
@@ -1,11 +1,14 @@
+import datetime
 import threading
+from timeit import default_timer as timer
 
 import numpy as np
 
 res = []
 
+
 def work_split(nrTasks, nrWorkers):
-    return (nrTasks / nrWorkers, nrTasks % nrWorkers)
+    return (nrTasks // nrWorkers, nrTasks % nrWorkers)
 
 
 def user_input():
@@ -15,6 +18,7 @@ def user_input():
     M = int(input("M= "))
     global res
     res = [[0 for _ in range(N)] for _ in range(M)]
+    solve(nrWorkers, N, K, M)
 
 
 def worker(matrix1, matrix2, a, b):
@@ -24,14 +28,30 @@ def worker(matrix1, matrix2, a, b):
 def solve(nrWorkers, N, K, M):
     matrix1 = np.random.random_integers(100, size=(N, K))
     matrix2 = np.random.random_integers(100, size=(K, M))
-    print(matrix1, "\n", matrix2)
-    interval, lastInterval = work_split(N, nrWorkers)
+    interval, rest = work_split(N, nrWorkers)
     threads = []
+
+    start1 = timer()
+    res2 = np.dot(matrix1, matrix2)
+    end1 = timer()
+
+    start2 = timer()
     for i in range(nrWorkers - 1):
+        if rest > 0:
+            t = threading.Thread(target=worker, args=(matrix1, matrix2, i * (interval + 1), (i + 1) * (interval + 1)))
+            threads.append(t)
+            t.start()
+            rest -= 1
         t = threading.Thread(target=worker, args=(matrix1, matrix2, i * interval, (i + 1) * interval))
         threads.append(t)
         t.start()
-    t = threading.Thread(target=np.dot,
-                         args=(matrix1[(nrWorkers - 1) * interval:(nrWorkers - 1) * interval + lastInterval], matrix2))
-    threads.append(t)
-    t.start()
+
+    for t in threads:
+        t.join()
+    end2 = timer()
+
+    print("Without threads: ", end1 - start1)
+    print("With threads: ", end2 - start2)
+
+
+user_input()
